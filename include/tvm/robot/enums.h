7f27be45704de329fbe0752d1420061d00c3e5a0
Pierre Gergondet <pierre.gergondet@gmail.com>
2017-11-24 16:10:11 +0900

    Add documentation for the tvm::robot enums
    Although they are not used yet this should give an insight into their
    intent.

--- include/tvm/robot/enums.h
+++ include/tvm/robot/enums.h
@@ -24,11 +24,38 @@ namespace tvm
 namespace robot
 {
 
+  /** For a given contact, specify the type of constraint that is applied:
+   *
+   * - Acceleration: constraint the two frames relative-acceleration to remain
+   *   the same
+   * - Velocity: constraint the two frames relative-velocity to remain the same
+   * - Position: constrant the two frames relative-position to remain the same
+   *
+   * This is done by specifiying different coefficient to the task's dynamics.
+   *
+   * This only applies to contacts that include a geometric component.
+   *
+   */
   enum class ContactConstraintType
   {
     Acceleration,
     Velocity,
     Position
+  };
+
+  /** Specify the type of contact:
+   *
+   * - Regular: both Force and Geometric
+   * - Force: force-only contact (no geometric constraint)
+   * - Geometric: geometric-only contact (no additional forces applied by the
+   *   robots onto each other)
+   *
+   */
+  enum class ContactType
+  {
+    Regular,
+    Force,
+    Geometric
   };
 
 }
