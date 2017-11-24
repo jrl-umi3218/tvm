7760b4427de89125097ce061dfe11efea8d27376
Pierre Gergondet <pierre.gergondet@gmail.com>
2017-11-24 17:13:24 +0900

    Add tvm::robot::CoMFunction

---
+++ include/tvm/robot/CoMFunction.h
@@ -0,0 +1,72 @@
+#pragma once
+
+/* Copyright 2017 CNRS-UM LIRMM, CNRS-AIST JRL
+ *
+ * This file is part of TVM.
+ *
+ * TVM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TVM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TVM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <tvm/Robot.h>
+
+#include <tvm/function/abstract/Function.h>
+
+#include <RBDyn/CoM.h>
+
+namespace tvm
+{
+
+namespace robot
+{
+
+  /** This class implements a CoM function for a given robot */
+  class TVM_DLLAPI CoMFunction : public function::abstract::Function
+  {
+  public:
+    SET_UPDATES(CoMFunction, Value, Velocity, Jacobian, NormalAcceleration, JDot)
+
+    /** Constructor
+     *
+     * Set the objective to the current CoM of robot
+     *
+     */
+    CoMFunction(RobotPtr robot);
+
+    /** Set the target posture to the current robot's CoM */
+    void reset();
+
+    /** Get the current objective */
+    inline const Eigen::Vector3d & com() const { return com_; }
+
+    /** Set the objective */
+    inline void com(const Eigen::Vector3d & com) { com_ = com; }
+  protected:
+    void updateValue();
+    void updateVelocity();
+    void updateJacobian();
+    void updateNormalAcceleration();
+    void updateJDot();
+
+    RobotPtr robot_;
+
+    /** Target */
+    Eigen::Vector3d com_;
+
+    /** CoM jacobian */
+    rbd::CoMJacobian jac_;
+  };
+
+}
+
+}
---
+++ src/robot/CoMFunction.cpp
@@ -0,0 +1,65 @@
+#include <tvm/robot/CoMFunction.h>
+
+namespace tvm
+{
+
+namespace robot
+{
+
+CoMFunction::CoMFunction(RobotPtr robot)
+: function::abstract::Function(3),
+  robot_(robot),
+  jac_(robot_->mb())
+{
+  reset();
+  registerUpdates(Update::Value, &CoMFunction::updateValue,
+                  Update::Velocity, &CoMFunction::updateVelocity,
+                  Update::Jacobian, &CoMFunction::updateJacobian,
+                  Update::NormalAcceleration, &CoMFunction::updateNormalAcceleration,
+                  Update::JDot, &CoMFunction::updateJDot);
+  addOutputDependency<CoMFunction>(Output::Value, Update::Value);
+  addOutputDependency<CoMFunction>(Output::Velocity, Update::Velocity);
+  addOutputDependency<CoMFunction>(Output::Jacobian, Update::Jacobian);
+  addOutputDependency<CoMFunction>(Output::NormalAcceleration, Update::NormalAcceleration);
+  addOutputDependency<CoMFunction>(Output::JDot, Update::JDot);
+  addVariable(robot_->q(), false);
+  addInputDependency<CoMFunction>(Update::Value, robot_, Robot::Output::CoM);
+  addInputDependency<CoMFunction>(Update::Velocity, robot_, Robot::Output::q);
+  addInputDependency<CoMFunction>(Update::Jacobian, robot_, Robot::Output::q);
+  addInputDependency<CoMFunction>(Update::NormalAcceleration, robot_, Robot::Output::q);
+  addInputDependency<CoMFunction>(Update::JDot, robot_, Robot::Output::q);
+}
+
+void CoMFunction::reset()
+{
+  com_ = rbd::computeCoM(robot_->mb(), robot_->mbc());
+}
+
+void CoMFunction::updateValue()
+{
+  value_ = robot_->com() - com_;
+}
+
+void CoMFunction::updateVelocity()
+{
+  velocity_ = jac_.velocity(robot_->mb(), robot_->mbc());
+}
+
+void CoMFunction::updateJacobian()
+{
+  jacobian_[robot_->q().get()] = jac_.jacobian(robot_->mb(), robot_->mbc());
+}
+
+void CoMFunction::updateNormalAcceleration()
+{
+  normalAcceleration_ = jac_.normalAcceleration(robot_->mb(), robot_->mbc(), robot_->normalAccB());
+}
+
+void CoMFunction::updateJDot()
+{
+  JDot_[robot_->q().get()] = jac_.jacobianDot(robot_->mb(), robot_->mbc());
+}
+
+} // namespace robot
+
+} // namespace tvm
