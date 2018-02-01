41c2a3ae00818cff831a47b5aace23b2a7f9deb3
Adrien Escande <adrien.escande@gmail.com>
2018-02-01 13:32:30 +0900

    Improve documentation in FunctionCheck.h

--- include/tvm/utils/FunctionCheck.h
+++ include/tvm/utils/FunctionCheck.h
@@ -26,6 +26,13 @@ namespace tvm
 
 namespace utils
 {
+  /** A small structure to specify options for the following functions.
+    *  - \a step is the increment that will be taken for finite difference schemes
+    *  - \a prec is the precision with which the equality of two vectors is tested.
+    *    It corresponds to the \a prec parameter of Eigen's \a isApprox method.
+    *  - if \a verbose is true, the functions will display some indications when an
+    *    mismatch is detected.
+    */
   class CheckOptions
   {
   public:
@@ -36,13 +43,20 @@ namespace utils
     bool verbose;
   };
 
-  /** Check the jacobian matrices of function f by forward finite differences.*/
+  /** Check the jacobian matrices of function \a f by forward finite differences.*/
   bool TVM_DLLAPI checkJacobian(FunctionPtr f, CheckOptions opt = CheckOptions());
 
-  /** Check the velocity of the function by comparing it to J*x. Assume that the
-    * jacobian matrices are correct.
+  /** Check the velocity of the function \a f by comparing it to J*x.
+    * Assume that the jacobian matrices are correct.
     */
   bool TVM_DLLAPI checkVelocity(FunctionPtr f, CheckOptions opt = CheckOptions());
+
+  /** Check the normal acceleration of the function \a f.
+    * Noting v=f(x), this is done by comparing it to ddot{v}-J\ddot{x}, where
+    * \ddot{x} is taken constant over the interval opt.step, and ddot{v} is obtained
+    * by finite differences.
+    * Assume that the jacobian matrices and the velocity are correct.
+    */
   bool TVM_DLLAPI checkNormalAcceleration(FunctionPtr f, CheckOptions opt = CheckOptions());
 }
 
