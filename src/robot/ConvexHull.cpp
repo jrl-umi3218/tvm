48f05bc838ad855d990043a34890e1120b87788c
Pierre Gergondet <pierre.gergondet@gmail.com>
2018-02-14 11:27:44 +0900

    Add tvm::robot::ConvexHull

---
+++ src/robot/ConvexHull.cpp
@@ -0,0 +1,41 @@
+#include <tvm/robot/ConvexHull.h>
+
+#include <tvm/utils/sch.h>
+
+namespace tvm
+{
+
+namespace robot
+{
+
+ConvexHull::ConvexHull(const std::string & path,
+                       FramePtr f, const sva::PTransformd & X_f_o)
+: ConvexHull(tvm::utils::Polyhedron(path), f, X_f_o)
+{
+}
+
+ConvexHull::ConvexHull(std::shared_ptr<sch::S_Object> o,
+                       FramePtr f, const sva::PTransformd & X_f_o)
+: o_(o), f_(f), X_f_o_(X_f_o)
+{
+  registerUpdates(Update::Position, &ConvexHull::updatePosition);
+  addOutputDependency(Output::Position, Update::Position);
+  addInputDependency(Update::Position, f_, Frame::Output::Position);
+
+  // Make sure the convex has the right position from the start
+  updatePosition();
+}
+
+sch::CD_Pair ConvexHull::makePair(const ConvexHull & hull) const
+{
+  return sch::CD_Pair(o_.get(), hull.o_.get());
+}
+
+void ConvexHull::updatePosition()
+{
+  tvm::utils::transform(*o_, X_f_o_ * f_->position());
+}
+
+} // namespace robot
+
+} // namespace tvm
