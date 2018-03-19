dddd81f8889e3033300de4eccfadde63afb7942f
Adrien Escande <adrien.escande@gmail.com>
2018-03-19 17:12:19 +0900

    Generalize MatrixWithProperties.h as a decorator for Eigen's objects

--- include/tvm/internal/MatrixWithProperties.h
+++ include/tvm/internal/MatrixWithProperties.h
@@ -28,7 +28,8 @@ namespace tvm
 namespace internal
 {
   //forward declaration
-  class MatrixWithProperties;
+  template<typename MatrixType>
+  class ObjectWithProperties;
 
   /** A lightweight proxy for indicating if the assignement of an Eigen
     * expression to a MatrixWithProperties should reset the properties (which is
@@ -36,6 +37,7 @@ namespace internal
     *
     * FIXME? Have a version which is compatible with Eigen::NoAlias (if needed).
     */
+  template<typename MatrixType>
   class KeepProperties
   {
   public:
@@ -44,71 +46,73 @@ namespace internal
       * \param M The MatrixWithProperties
       * \param keep Specified is the properties should be kept or not
       */
-    KeepProperties(MatrixWithProperties& M, bool keep) : M_(M), keep_(keep) {}
+    KeepProperties(ObjectWithProperties<MatrixType>& M, bool keep) : M_(M), keep_(keep) {}
 
     /** The assignment operator for Eigen's expression*/
     template<typename OtherDerived>
-    MatrixWithProperties& operator=(const Eigen::MatrixBase<OtherDerived>& other);
+    ObjectWithProperties<MatrixType>& operator=(const Eigen::MatrixBase<OtherDerived>& other);
 
     /** We delete this operator so that it cannot be used for the classical copy
       * operator of MatrixWithProperties. */
-    MatrixWithProperties& operator=(const MatrixWithProperties&) = delete;
+    ObjectWithProperties<MatrixType>& operator=(const ObjectWithProperties<MatrixType>&) = delete;
 
   private:
-    MatrixWithProperties& M_;
+    ObjectWithProperties<MatrixType>& M_;
     const bool keep_;
   };
 
   /** An Eigen matrix together with MatrixProperties. */
-  class MatrixWithProperties : public Eigen::MatrixXd
+  template<typename MatrixType>
+  class ObjectWithProperties : public MatrixType
   {
   public:
-    using Eigen::MatrixXd::MatrixXd;
+    using MatrixType::MatrixType;
 
-    MatrixWithProperties() {}
+    ObjectWithProperties() {}
 
     template<typename OtherDerived>
-    MatrixWithProperties(const Eigen::MatrixBase<OtherDerived>& other, const MatrixProperties& p)
-      : Eigen::MatrixXd(other), properties_(p)
+    ObjectWithProperties(const Eigen::MatrixBase<OtherDerived>& other, const MatrixProperties& p)
+      : T(other), properties_(p)
     {
     }
 
     template<typename OtherDerived>
-    MatrixWithProperties& operator=(const Eigen::MatrixBase<OtherDerived>& other)
+    ObjectWithProperties& operator=(const Eigen::MatrixBase<OtherDerived>& other)
     {
       assert(this->rows() == other.rows() && this->cols() == other.cols()
         && "It is not allowed to assign an expression with a different size. Please explicitely resize the matrix before.");
-      this->Eigen::MatrixXd::operator=(other);
+      this->MatrixType::operator=(other);
       properties_ = MatrixProperties();
       return *this;
     }
 
     template<typename OtherDerived>
-    MatrixWithProperties& assignKeepProperties(const Eigen::MatrixBase<OtherDerived>& other)
+    ObjectWithProperties& assignKeepProperties(const Eigen::MatrixBase<OtherDerived>& other)
     {
       assert(this->rows() == other.rows() && this->cols() == other.cols()
         && "It is not allowed to assign an expression with a different size. Please explicitely resize the matrix before.");
-      this->Eigen::MatrixXd::operator=(other);
+      this->MatrixType::operator=(other);
       return *this;
     }
 
     const MatrixProperties& properties() const { return properties_; }
     void properties(const MatrixProperties& p) { properties_ = p; }
 
-    /** Create a proxy to specfy wether an assignement should preserve the
+    /** Create a proxy to specify wether an assignement should preserve the
       * properties of the matrix. 
       *
       * \param keep true it the properties should be left untouched, false
       * otherwise.
       */
-    KeepProperties keepProperties(bool keep) { return { *this, keep }; }
+    KeepProperties<MatrixType> keepProperties(bool keep) { return { *this, keep }; }
 
   private:
     MatrixProperties properties_;
   };
 
+  template<typename MatrixType>
   template<typename OtherDerived>
-  inline MatrixWithProperties& KeepProperties::operator=(const Eigen::MatrixBase<OtherDerived>& other)
+  inline ObjectWithProperties<MatrixType>& KeepProperties<MatrixType>::operator=(const Eigen::MatrixBase<OtherDerived>& other)
   {
     if (keep_)
     {
@@ -120,6 +124,9 @@ namespace internal
     }
     return M_;
   }
+
+  using MatrixWithProperties = ObjectWithProperties<Eigen::MatrixXd>;
+  using VectorWithProperties = ObjectWithProperties<Eigen::VectorXd>;
 
 }  // namespace internal
 
