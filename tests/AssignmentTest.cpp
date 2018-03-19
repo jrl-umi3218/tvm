d4b38d5d875cdbd704a25f8ba90336f71ad9ca31
Adrien Escande <adrien.escande@gmail.com>
2018-03-19 17:16:43 +0900

    test substitution assignment

--- tests/AssignmentTest.cpp
+++ tests/AssignmentTest.cpp
@@ -9,6 +9,8 @@
 #include <tvm/Variable.h>
 #include <tvm/VariableVector.h>
 #include <tvm/constraint/BasicLinearConstraint.h>
+#include <tvm/hint/internal/Substitutions.h>
+#include <tvm/hint/Substitution.h>
 #include <tvm/scheme/internal/Assignment.h>
 #include <tvm/scheme/internal/AssignmentTarget.h>
 
@@ -20,6 +22,8 @@ static const double large = tvm::constant::big_number;
 
 using namespace tvm;
 using namespace tvm::constraint;
+using namespace tvm::hint;
+using namespace tvm::hint::internal;
 using namespace tvm::scheme::internal;
 using namespace tvm::requirements;
 using namespace Eigen;
@@ -78,7 +82,7 @@ struct Memory
 bool check(BLCPtr c, const VectorXd& x)
 {
   const double eps = 1e-12;
-  c->variables()[0]->value(x);
+  const_cast<VariableVector&>(c->variables()).value(x);
   c->updateValue();
   auto v = c->value();
   if (c->type() == Type::DOUBLE_SIDED)
@@ -232,6 +236,56 @@ Constraints buildSimpleConstraints(bool minus = false, VariablePtr y = nullptr)
   return cstr;
 }
 
+//create a set of constraints -3x+4y op +/-2 and a constraint for substitution
+// -x + 2y - z = 0/1/-1 (the latter value depending on the rhs param).
+std::pair<Constraints, BLCPtr> buildSimpleSubstitution(RHS rhs)
+{
+  Constraints cstr;
+  VariablePtr x = Space(1).createVariable("x");
+  VariablePtr y = Space(1).createVariable("y");
+  VariablePtr z = Space(1).createVariable("z");
+
+  //generate matrix
+  MatrixXd I = MatrixXd::Identity(1, 1);
+  MatrixXd Ax = -3 * I;
+  MatrixXd Ay = 4 * I;
+  VectorXd l = VectorXd::Constant(1, -2);
+  VectorXd u = VectorXd::Constant(1, 2);
+
+  std::vector<Ref<const MatrixXd>> A = { Ax, Ay };
+  std::vector<VariablePtr> v = { x, y };
+
+  cstr.Ax_eq_0 = std::make_shared<BasicLinearConstraint>( A, v, Type::EQUAL);
+  cstr.Ax_geq_0 = std::make_shared<BasicLinearConstraint>(A, v, Type::GREATER_THAN);
+  cstr.Ax_leq_0 = std::make_shared<BasicLinearConstraint>(A, v, Type::LOWER_THAN);
+
+  cstr.Ax_eq_b = std::make_shared<BasicLinearConstraint>( A, v, l, Type::EQUAL);
+  cstr.Ax_geq_b = std::make_shared<BasicLinearConstraint>(A, v, l, Type::GREATER_THAN);
+  cstr.Ax_leq_b = std::make_shared<BasicLinearConstraint>(A, v, u, Type::LOWER_THAN);
+
+  cstr.Ax_eq_minus_b = std::make_shared<BasicLinearConstraint>( A, v, -l, Type::EQUAL, RHS::OPPOSITE);
+  cstr.Ax_geq_minus_b = std::make_shared<BasicLinearConstraint>(A, v, -l, Type::GREATER_THAN, RHS::OPPOSITE);
+  cstr.Ax_leq_minus_b = std::make_shared<BasicLinearConstraint>(A, v, -u, Type::LOWER_THAN, RHS::OPPOSITE);
+
+  cstr.l_leq_Ax_leq_u = std::make_shared<BasicLinearConstraint>(A, v, l, u);
+  cstr.minus_l_leq_Ax_leq_minus_u = std::make_shared<BasicLinearConstraint>(A, v, -l, -u, RHS::OPPOSITE);
+
+  MatrixXd Cx = -I;
+  MatrixXd Cy = 2 * I;
+  MatrixXd Cz = -I;
+  std::vector<Ref<const MatrixXd>> C = { Cx, Cy, Cz };
+  std::vector<VariablePtr> w = { x, y, z };
+  VectorXd d = VectorXd::Constant(1, 1);
+  if (rhs == RHS::ZERO)
+  {
+    return std::make_pair(cstr, std::make_shared<BasicLinearConstraint>(C, w, Type::EQUAL));
+  }
+  else
+  {
+    return std::make_pair(cstr, std::make_shared<BasicLinearConstraint>(C, w, d, Type::EQUAL, rhs));
+  }
+}
+
 //check that each point -3, -2, -1, 0, 1, 2 and 3 is either verifying at the same time
 //the two descriptions of the constraint or violating both of them.
 void checkSimple(BLCPtr cstr, const Memory& mem, Type t, RHS r, bool bound = false)
@@ -276,6 +330,50 @@ void checkSimple(BLCPtr cstr1, BLCPtr cstr2, const Memory& mem)
   FAST_CHECK_EQ(check(cstr1, p3) && check(cstr2, p3), check(mem, t, r, p3, true));
 }
 
+// Check that each point (x,z) with z=-x, for x = -5,-3,1,0,1,3,5 verifies or violates the
+// substituted constraint in mem at the same time as the corresponding (x, y) point verifies
+// or violates the constraint.
+void checkSubstitution(BLCPtr cstr, RHS subRhs, const Memory& mem, Type t, RHS r)
+{
+  double y;
+  switch (subRhs)
+  {
+  case RHS::ZERO: y = 0; break;
+  case RHS::AS_GIVEN: y = 0.5; break;
+  case RHS::OPPOSITE: y = -0.5; break;
+  }
+  VectorXd xym5(2); xym5 << -2, y;
+  VectorXd xym3(2); xym3 << -4./3, y;
+  VectorXd xym1(2); xym1 << -2./3, y;
+  VectorXd xy0(2); xy0 << 0, y;
+  VectorXd xy1(2); xy1 << 2./3, y;
+  VectorXd xy3(2); xy3 << 4./3, y;
+  VectorXd xy5(2); xy5 << 2, y;
+  VectorXd xzm5(2); xzm5 << -2, 2;
+  VectorXd xzm3(2); xzm3 << -4./3, 4./3;
+  VectorXd xzm1(2); xzm1 << -2./3, 2./3;
+  VectorXd xz0(2); xz0 << 0, 0;
+  VectorXd xz1(2); xz1 << 2./3, -2./3;
+  VectorXd xz3(2); xz3 << 4./3, -4./3;
+  VectorXd xz5(2); xz5 << 2, -2;
+
+  //std::cout << check(cstr, xym5) <<", " << check(mem, t, r, xzm5, false) << std::endl;
+  //std::cout << check(cstr, xym3) <<", " << check(mem, t, r, xzm3, false) << std::endl;
+  //std::cout << check(cstr, xym1) <<", " << check(mem, t, r, xzm1, false) << std::endl;
+  //std::cout << check(cstr, xy0)  <<", " << check(mem, t, r, xz0, false)  << std::endl;
+  //std::cout << check(cstr, xy1)  <<", " << check(mem, t, r, xz1, false)  << std::endl;
+  //std::cout << check(cstr, xy3)  <<", " << check(mem, t, r, xz3, false)  << std::endl;
+  //std::cout << check(cstr, xy5)  <<", " << check(mem, t, r, xz5, false)  << std::endl;
+
+  FAST_CHECK_EQ(check(cstr, xym5), check(mem, t, r, xzm5, false));
+  FAST_CHECK_EQ(check(cstr, xym3), check(mem, t, r, xzm3, false));
+  FAST_CHECK_EQ(check(cstr, xym1), check(mem, t, r, xzm1, false));
+  FAST_CHECK_EQ(check(cstr, xy0), check(mem, t, r, xz0, false));
+  FAST_CHECK_EQ(check(cstr, xy1), check(mem, t, r, xz1, false));
+  FAST_CHECK_EQ(check(cstr, xy3), check(mem, t, r, xz3, false));
+  FAST_CHECK_EQ(check(cstr, xy5), check(mem, t, r, xz5, false));
+}
+
 void checkAssignment(BLCPtr c, const AssignmentTarget& at, Memory& mem, Type t, RHS r, bool throws)
 {
   auto req = std::make_shared<SolvingRequirements>();
@@ -290,6 +388,25 @@ void checkAssignment(BLCPtr c, const AssignmentTarget& at, Memory& mem, Type t, 
     Assignment a(c, req, at, vars);
     a.run();
     checkSimple(c, mem, t, r);
+  }
+}
+
+void checkSubstitutionAssignment(BLCPtr c, Substitutions& s, const AssignmentTarget& at, Memory& mem, Type t, RHS r, bool throws)
+{
+  auto req = std::make_shared<SolvingRequirements>();
+  VariableVector vars;
+  vars.add(s.otherVariables());
+  mem.randomize();
+  if (throws)
+  {
+    CHECK_THROWS(Assignment a(c, req, at, vars, s));
+  }
+  else
+  {
+    s.updateSubstitutions();
+    Assignment a(c, req, at, vars, s);
+    a.run();
+    checkSubstitution(c, s.substitutions()[0].constraints()[0]->rhs(), mem, t, r);
   }
 }
 
@@ -405,6 +522,101 @@ void checkSimple(BLCPtr c, std::vector<bool> throws)
   }
 }
 
+//same as above but introducing a substitution
+void checkSimple(BLCPtr c, const Substitution& sub, std::vector<bool> throws)
+{
+  int s = c->type() == Type::DOUBLE_SIDED ? 2 : 1;
+  auto sMem = std::make_shared<Memory>(s, 2);
+  auto sRange = std::make_shared<Range>(0, s);
+  auto dMem = std::make_shared<Memory>(1, 2);
+  auto dRange = std::make_shared<Range>(0, 1);
+
+  Substitutions subs;
+  subs.add(sub);
+  subs.finalize();
+
+  // target Cx=0
+  {
+    auto t = Type::EQUAL;
+    auto r = RHS::ZERO;
+    AssignmentTarget at(sRange, sMem->A, t);
+    checkSubstitutionAssignment(c, subs, at, *sMem, t, r, throws[0]);
+  }
+  // target Cx=d
+  {
+    auto t = Type::EQUAL;
+    auto r = RHS::AS_GIVEN;
+    AssignmentTarget at(sRange, sMem->A, sMem->b, t, r);
+    checkSubstitutionAssignment(c, subs, at, *sMem, t, r, throws[1]);
+  }
+  // target Cx=-d
+  {
+    auto t = Type::EQUAL;
+    auto r = RHS::OPPOSITE;
+    sMem->randomize();
+    AssignmentTarget at(sRange, sMem->A, sMem->b, t, r);
+    checkSubstitutionAssignment(c, subs, at, *sMem, t, r, throws[2]);
+  }
+  // target Cx>=0
+  {
+    auto t = Type::GREATER_THAN;
+    auto r = RHS::ZERO;
+    AssignmentTarget at(sRange, sMem->A, t);
+    checkSubstitutionAssignment(c, subs, at, *sMem, t, r, throws[3]);
+  }
+  // target Cx>=d
+  {
+    auto t = Type::GREATER_THAN;
+    auto r = RHS::AS_GIVEN;
+    AssignmentTarget at(sRange, sMem->A, sMem->b, t, r);
+    checkSubstitutionAssignment(c, subs, at, *sMem, t, r, throws[4]);
+  }
+  // target Cx>=-d
+  {
+    auto t = Type::GREATER_THAN;
+    auto r = RHS::OPPOSITE;
+    sMem->randomize();
+    AssignmentTarget at(sRange, sMem->A, sMem->b, t, r);
+    checkSubstitutionAssignment(c, subs, at, *sMem, t, r, throws[5]);
+  }
+  // target Cx<=0
+  {
+    auto t = Type::LOWER_THAN;
+    auto r = RHS::ZERO;
+    AssignmentTarget at(sRange, sMem->A, t);
+    checkSubstitutionAssignment(c, subs, at, *sMem, t, r, throws[6]);
+  }
+  // target Cx<=d
+  {
+    auto t = Type::LOWER_THAN;
+    auto r = RHS::AS_GIVEN;
+    AssignmentTarget at(sRange, sMem->A, sMem->b, t, r);
+    checkSubstitutionAssignment(c, subs, at, *sMem, t, r, throws[7]);
+  }
+  // target Cx<=-d
+  {
+    auto t = Type::LOWER_THAN;
+    auto r = RHS::OPPOSITE;
+    sMem->randomize();
+    AssignmentTarget at(sRange, sMem->A, sMem->b, t, r);
+    checkSubstitutionAssignment(c, subs, at, *sMem, t, r, throws[8]);
+  }
+  // target l<=Cx<=u
+  {
+    auto t = Type::DOUBLE_SIDED;
+    auto r = RHS::AS_GIVEN;
+    AssignmentTarget at(dRange, dMem->A, dMem->l, dMem->u, r);
+    checkSubstitutionAssignment(c, subs, at, *dMem, t, r, throws[9]);
+  }
+  // target -l<=Cx<=-u
+  {
+    auto t = Type::DOUBLE_SIDED;
+    auto r = RHS::AS_GIVEN;
+    AssignmentTarget at(dRange, dMem->A, dMem->l, dMem->u, r);
+    checkSubstitutionAssignment(c, subs, at, *dMem, t, r, throws[10]);
+  }
+}
+
 /** Check the assignement of a bound \p c to a target */
 void checkSimpleBound(BLCPtr c)
 {
@@ -426,7 +638,7 @@ void checkSimpleBound(BLCPtr c1, BLCPtr c2)
   checkBoundAssignment(c1, c2, at, *dMem);
 }
 
-//test for correct signs on the matrices and vector
+// test for correct signs on the matrices and vector
 TEST_CASE("Test simple assignment")
 {
   Constraints cstr = buildSimpleConstraints();
@@ -542,6 +754,115 @@ TEST_CASE("Test simple assignment")
       checkSimpleBound(c1[i], c1[j]);
       checkSimpleBound(c1[i], c2[j]);
     }
+  }
+}
+
+TEST_CASE("Test assignements with substitution")
+{
+  auto p0 = buildSimpleSubstitution(RHS::ZERO);
+  auto p1 = buildSimpleSubstitution(RHS::AS_GIVEN);
+  auto p2 = buildSimpleSubstitution(RHS::OPPOSITE);
+  Substitution s0(p0.second, p0.second->variables()[1]); //substitution of y
+  Substitution s1(p1.second, p1.second->variables()[1]); //substitution of y
+  Substitution s2(p2.second, p2.second->variables()[1]); //substitution of y
+  
+  using T = std::vector<bool>;
+  const bool t = true;
+  const bool f = false;
+  // constraint Ax = 0
+  {
+    //      Cx=0,  Cx=d,  Cx=-d,  Cx>=0,  Cx>=d, Cx>=-d,  Cx<=0,  Cx<=d, Cx<=-d,l<=Cx<=u,-l<=Cx<=-u
+    T v0 = { f,      f,      f,      t,      t,      t,      t,      t,      t,      f,      f };
+    T v1 = { t,      f,      f,      t,      t,      t,      t,      t,      t,      f,      f };
+    T v2 = { t,      f,      f,      t,      t,      t,      t,      t,      t,      f,      f };
+
+    checkSimple(p0.first.Ax_eq_0, s0, v0);
+    checkSimple(p1.first.Ax_eq_0, s1, v1);
+    checkSimple(p2.first.Ax_eq_0, s2, v2);
+  }
+
+  // constraint Ax >= 0
+  {
+    //      Cx=0,  Cx=d,  Cx=-d,  Cx>=0,  Cx>=d, Cx>=-d,  Cx<=0,  Cx<=d, Cx<=-d,l<=Cx<=u,-l<=Cx<=-u
+    T v0 = { t,      t,      t,      f,      f,      f,      f,      f,      f,      f,      f };
+    T v1 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+    T v2 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+
+    checkSimple(p0.first.Ax_geq_0, s0, v0);
+    checkSimple(p1.first.Ax_geq_0, s1, v1);
+    checkSimple(p2.first.Ax_geq_0, s2, v2);
+  }
+
+  // constraint Ax <= 0
+  {
+    //      Cx=0,  Cx=d,  Cx=-d,  Cx>=0,  Cx>=d, Cx>=-d,  Cx<=0,  Cx<=d, Cx<=-d,l<=Cx<=u,-l<=Cx<=-u
+    T v0 = { t,      t,      t,      f,      f,      f,      f,      f,      f,      f,      f };
+    T v1 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+    T v2 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+
+    checkSimple(p0.first.Ax_leq_0, s0, v0);
+    checkSimple(p1.first.Ax_leq_0, s1, v1);
+    checkSimple(p2.first.Ax_leq_0, s2, v2);
+  }
+
+  // constraint Ax = 0
+  {
+    //      Cx=0,  Cx=d,  Cx=-d,  Cx>=0,  Cx>=d, Cx>=-d,  Cx<=0,  Cx<=d, Cx<=-d,l<=Cx<=u,-l<=Cx<=-u
+    T v0 = { t,      f,      f,      t,      t,      t,      t,      t,      t,      f,      f };
+    T v1 = { t,      f,      f,      t,      t,      t,      t,      t,      t,      f,      f };
+    T v2 = { t,      f,      f,      t,      t,      t,      t,      t,      t,      f,      f };
+
+    checkSimple(p0.first.Ax_eq_b, s0, v0);
+    checkSimple(p1.first.Ax_eq_b, s1, v1);
+    checkSimple(p2.first.Ax_eq_b, s2, v2);
+  }
+
+  // constraint Ax >= b
+  {
+    //      Cx=0,  Cx=d,  Cx=-d,  Cx>=0,  Cx>=d, Cx>=-d,  Cx<=0,  Cx<=d, Cx<=-d,l<=Cx<=u,-l<=Cx<=-u
+    T v0 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+    T v1 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+    T v2 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+
+    checkSimple(p0.first.Ax_geq_b, s0, v0);
+    checkSimple(p1.first.Ax_geq_b, s1, v1);
+    checkSimple(p2.first.Ax_geq_b, s2, v2);
+  }
+
+  // constraint Ax <= b
+  {
+    //      Cx=0,  Cx=d,  Cx=-d,  Cx>=0,  Cx>=d, Cx>=-d,  Cx<=0,  Cx<=d, Cx<=-d,l<=Cx<=u,-l<=Cx<=-u
+    T v0 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+    T v1 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+    T v2 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+
+    checkSimple(p0.first.Ax_leq_b, s0, v0);
+    checkSimple(p1.first.Ax_leq_b, s1, v1);
+    checkSimple(p2.first.Ax_leq_b, s2, v2);
+  }
+
+  // constraint Ax >= b
+  {
+    //      Cx=0,  Cx=d,  Cx=-d,  Cx>=0,  Cx>=d, Cx>=-d,  Cx<=0,  Cx<=d, Cx<=-d,l<=Cx<=u,-l<=Cx<=-u
+    T v0 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+    T v1 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+    T v2 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+
+    checkSimple(p0.first.l_leq_Ax_leq_u, s0, v0);
+    checkSimple(p1.first.l_leq_Ax_leq_u, s1, v1);
+    checkSimple(p2.first.l_leq_Ax_leq_u, s2, v2);
+  }
+
+  // constraint Ax <= b
+  {
+    //      Cx=0,  Cx=d,  Cx=-d,  Cx>=0,  Cx>=d, Cx>=-d,  Cx<=0,  Cx<=d, Cx<=-d,l<=Cx<=u,-l<=Cx<=-u
+    T v0 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+    T v1 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+    T v2 = { t,      t,      t,      t,      f,      f,      t,      f,      f,      f,      f };
+
+    checkSimple(p0.first.minus_l_leq_Ax_leq_minus_u, s0, v0);
+    checkSimple(p1.first.minus_l_leq_Ax_leq_minus_u, s1, v1);
+    checkSimple(p2.first.minus_l_leq_Ax_leq_minus_u, s2, v2);
   }
 }
 
