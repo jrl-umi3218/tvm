57c677836ca1cafcd1a13daa7fcdfea9c3c8d7ed
Adrien Escande <adrien.escande@aist.go.jp>
2020-09-01 18:31:40 +0900

    First version of systematic tests for add/remove tasks events

--- tests/EventsTest.cpp
+++ tests/EventsTest.cpp
@@ -7,6 +7,8 @@
 #include <tvm/scheme/WeightedLeastSquares.h>
 #include <tvm/solver/defaultLeastSquareSolver.h>
 
+#include <array>
+
 using namespace tvm;
 using namespace Eigen;
 
@@ -105,4 +107,145 @@ TEST_CASE("Add/Remove variables from problem")
   pb.remove(t1.get());
   solver.solve(pb);
   FAST_CHECK_UNARY(y->value().isApprox(Vector3d(0, 0, 0)));
+}
+
+bool skip(const std::array<bool, 12>& a)
+{
+  int eq = a[0] + a[1] + a[2];
+  int ineq = a[3] + a[4] + a[5];
+  int bnd = a[6] + a[7] + a[8];
+  int obj = a[9] + a[10] + a[11];
+
+  int n = (eq > 1) + (ineq > 1) + (bnd > 1) + (obj > 1);
+  return n > 1;
+}
+
+void buildPb(LinearizedControlProblem& pb,
+             const std::array<TaskWithRequirementsPtr, 12>& tasks,
+             const std::array<bool, 12>& selection)
+{
+  for (int i = 0; i < 12; ++i)
+  {
+    if (selection[i]) pb.add(tasks[i]);
+  }
+}
+
+void test1Change(const std::array<TaskWithRequirementsPtr, 12>& tasks, const std::array<bool, 12>& selection)
+{
+  scheme::WeightedLeastSquares solver(solver::DefaultLSSolverOptions().verbose(false));
+
+  for (int i = 1; i < 12; ++i)
+  {
+    LinearizedControlProblem pb;
+    LinearizedControlProblem pbGroundTruth;
+    buildPb(pb, tasks, selection);
+    buildPb(pbGroundTruth, tasks, selection);
+
+    // We solve pb for the current list of tasks, but not pbGroundTruth
+    solver.solve(pb);
+    
+    if (selection[i])
+    {
+      pb.remove(tasks[i].get());
+      pbGroundTruth.remove(tasks[i].get());
+    }
+    else
+    {
+      pb.add(tasks[i]);
+      pbGroundTruth.add(tasks[i]);
+    }
+
+    // Now we solve both problem. Only pb is performing an update
+    FAST_CHECK_UNARY(solver.solve(pb));
+    VectorXd s = pb.variables().value();
+    FAST_CHECK_UNARY(solver.solve(pbGroundTruth));
+    VectorXd s0 = pbGroundTruth.variables().value();
+
+    Vector2d eps = Vector2d::Constant(1e-6);
+    for (int j = 0; j < 9; ++j) // all constraints
+    {
+      if ((selection[j] && i != j) || (!selection[j] && i == j))
+      {
+        auto t = tasks[j]->task;
+        auto f = std::static_pointer_cast<tvm::function::abstract::LinearFunction>(t.function());
+        pb.variables().value(s);
+        f->updateValue();
+        Vector2d v = f->value() - tasks[j]->task.taskDynamics<task_dynamics::None>()->value();
+        switch (t.type())
+        {
+        case constraint::Type::EQUAL: FAST_CHECK_UNARY(v.isZero()); break;
+        case constraint::Type::GREATER_THAN: FAST_CHECK_UNARY((v.array() >= -eps.array()).all()); break;
+        case constraint::Type::LOWER_THAN: FAST_CHECK_UNARY((v.array() <= eps.array()).all()); break;
+        case constraint::Type::DOUBLE_SIDED:
+        {
+          Vector2d v2 = f->value() - tasks[j]->task.secondBoundTaskDynamics<task_dynamics::None>()->value();
+          FAST_CHECK_UNARY((v.array() >= -eps.array()).all());
+          FAST_CHECK_UNARY((v2.array() <= eps.array()).all());
+        }
+        break;
+        }
+      }
+    }
+    for (int j = 9; j < 12; ++j) //objectives
+    {
+      if ((selection[j] && i != j) || (!selection[j] && i == j))
+      {
+        auto t = tasks[j]->task;
+        auto f = std::static_pointer_cast<tvm::function::abstract::LinearFunction>(t.function());
+        pbGroundTruth.variables().value(s0);
+        f->updateValue();
+        Vector2d obj0 = f->value();
+        pb.variables().value(s);
+        f->updateValue();
+        Vector2d obj = f->value();
+        FAST_CHECK_UNARY((obj0-obj).isZero());
+      }
+    }
+  }
+
+}
+
+TEST_CASE("Systematic add/remove of one task")
+{
+  Space R2(2);
+  VariablePtr x = R2.createVariable("x");
+  VariablePtr y = R2.createVariable("y");
+  VariablePtr z = R2.createVariable("z");
+
+  using task_dynamics::None;
+  SolvingRequirements P0 = { PriorityLevel(0) };
+  SolvingRequirements P1 = { PriorityLevel(1) };
+  // 3 of each: equality, inequality, bound and objective, in that order
+  std::array<TaskWithRequirementsPtr, 12> tasks = {
+  std::make_shared<TaskWithRequirements>(Task{ x + 2 * y == 3., None() }, P0),
+  std::make_shared<TaskWithRequirements>(Task{ 3 * x + z == 4., None() }, P0),
+  std::make_shared<TaskWithRequirements>(Task{ y - z == 0.,     None() }, P0),
+  std::make_shared<TaskWithRequirements>(Task{ x + y >= 0.,     None() }, P0),
+  std::make_shared<TaskWithRequirements>(Task{ x + y + z >= 1., None() }, P0),
+  std::make_shared<TaskWithRequirements>(Task{ x + z <= 3.,     None() }, P0),
+  std::make_shared<TaskWithRequirements>(Task{ 0. <= x <= 4.,   None() }, P0),
+  std::make_shared<TaskWithRequirements>(Task{ -2. <= x <= 2.,  None() }, P0),
+  std::make_shared<TaskWithRequirements>(Task{ x <= 3.,         None() }, P0),
+  std::make_shared<TaskWithRequirements>(Task{ x == 0.,         None() }, P1),
+  std::make_shared<TaskWithRequirements>(Task{ y == 0.,         None() }, P1),
+  std::make_shared<TaskWithRequirements>(Task{ z == 0.,         None() }, P1) };
+  //std::array<bool, 12> added = { true, true, true, true, false, false, false, false, false, false, false, true };
+  std::array<bool, 12> added = { false, false, false, true, false, false, false, false, false, false, false, false };
+  //std::array<bool, 12> added = {false, false, false, false, false, false, false, false, false, false, false, false};
+
+  // all combinations of true/false for added
+  for (int i = 1; i < 4096; ++i)
+  {
+    // Take the next value of added
+    for (int j = 0; j < 12; ++j)
+    {
+      added[j] = !added[j];
+      if (added[j]) break;
+    }
+    //for (int j = 0; j < 12; ++j)
+    //  std::cout << added[j] ? "1 " : "0 ";
+    //std::cout << "\n";
+    if (!skip(added))
+      test1Change(tasks, added);
+  }
 }
