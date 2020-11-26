6f8505f446c6cf3c8697b93696e58623af920d32
Adrien Escande <adrien.escande@aist.go.jp>
2020-11-26 10:03:19 +0900

    JDot with subvariables

--- tests/FunctionTest.cpp
+++ tests/FunctionTest.cpp
@@ -39,3 +39,30 @@ TEST_CASE("Test subvariable jacobians")
   CHECK_THROWS(f.jacobian(*w));
   tvm::utils::set_is_malloc_allowed(true);
 }
+
+TEST_CASE("Test subvariable JDot")
+{
+  VariablePtr x = Space(7).createVariable("x");
+  VariablePtr y = Space(8).createVariable("y");
+  VariablePtr z = Space(6).createVariable("z");
+  VariablePtr w = Space(4).createVariable("w");
+
+  VariablePtr x1 = x->subvariable(3, "x1", 1);
+  VariablePtr y1 = y->subvariable(6, "y1", 1);
+  VariablePtr y2 = y->subvariable(3, "y2", 2);
+
+  MatrixXd A = MatrixXd::Random(3, 7);
+  MatrixXd B = MatrixXd::Random(3, 6);
+  MatrixXd C = MatrixXd::Random(3, 6);
+
+  function::BasicLinearFunction f(A * x + B * y1 + C * z);
+
+  tvm::utils::set_is_malloc_allowed(false);
+  FAST_CHECK_EQ(f.JDot(*x).cols(), 7);
+  FAST_CHECK_EQ(f.JDot(*x1).cols(), 3);
+  FAST_CHECK_EQ(f.JDot(*y1).cols(), 6);
+  FAST_CHECK_EQ(f.JDot(*y2).cols(), 3);
+  CHECK_THROWS(f.JDot(*y));
+  CHECK_THROWS(f.JDot(*w));
+  tvm::utils::set_is_malloc_allowed(true);
+}
