32599c379b7344472f5058daa5f284e3216ea297
Adrien Escande <adrien.escande@aist.go.jp>
2020-12-22 16:39:52 +0900

    Additional comments and minor reorganization of if/else in RangeCounting.cpp

--- src/internal/RangeCounting.cpp
+++ src/internal/RangeCounting.cpp
@@ -15,28 +15,28 @@ bool RangeCounting::add(const Range & r)
     limits_.emplace_back(r.end(), Limit::Upper);
     return recompute(true);
   }
-  else
-  {
-    int depth = 0;
-    bool change = false;
-    Limit l = {r.start, Limit::Lower};
-    Limit u = {r.end(), Limit::Upper};
-    auto it = limits_.begin();
-    moveToFirstAfter(l, it, depth);
-    if(it == limits_.end())
-    {
-      limits_.push_back(l);
-      limits_.push_back(u);
-      return recompute(true);
-    }
-    insert(l, it, depth);
-    if(depth == 1)
-      change = true;
 
-    change = moveToFirstAfter(Limit(r.end(), Limit::Cut), it, depth, 1) || change;
-    insert(u, it, depth);
-    return recompute(change);
+  int depth = 0;
+  bool change = false;
+  Limit l = {r.start, Limit::Lower};
+  Limit u = {r.end(), Limit::Upper};
+  auto it = limits_.begin();
+  moveToFirstAfter(l, it, depth);
+  if(it == limits_.end())
+  {
+    limits_.push_back(l);
+    limits_.push_back(u);
+    return recompute(true);
   }
+  insert(l, it, depth);
+  if(depth == 1)
+    change = true;
+
+  // We have inserted a lower limit, raising the depth by one. If we encounter a depth equal to 1,
+  // this means that it was previously 0, and we are joining to previously separate ranges.
+  change = moveToFirstAfter(Limit(r.end(), Limit::Cut), it, depth, 1) || change;
+  insert(u, it, depth);
+  return recompute(change);
 }
 
 bool RangeCounting::remove(const Range & r)
@@ -56,16 +56,23 @@ bool RangeCounting::remove(const Range & r)
   Limit l(r.start, Limit::Upper);
   Limit u(r.end(), Limit::Lower);
   auto it = limits_.begin();
+  // Finding where to insert l
   moveToFirstAfter({r.start, Limit::Cut}, it, depth);
   if(depth == 0)
     throw std::runtime_error("Trying to remove from a non-existing interval");
   bool change = (depth == 1);
-  auto it1 = it; // We don't insert the value yet, to keep a valid state in case a exception is thrown
-  int depth1 = depth;
+  // We don't insert the value yet, to keep a valid state in case an exception is thrown
+  // it1 is the place where we need to insert l
+  auto it1 = it;
+  int depth1 = depth; // we also need to remember the depth correspondind to it1
+  // `it` becomes the place where we need to insert u. We didn't insert l yet so the depth is one higher
+  // than what we'll have. So if we encounter depth 1, it means that we will get separate ranges after.
   change = moveToFirstAfter(u, it, depth, 1) || change;
-  insert(l, it1, depth1);
-  --depth;
+  insert(l, it1, depth1); // we're using depth1 here as it is the depth at it1
+  --depth; // but we need to reflect the insertion of l on depth too (the above line does it only for depth1)
   insert(u, it, depth);
+
+  // Scan limits_ and remove any limit of type Limit::Cut with depth 0 (enforcing rule (6))
   depth = 0;
   it = limits_.begin();
   while(it != limits_.end()) // ideally from it1 to it, but the second insert can invalidate it1
@@ -82,62 +89,64 @@ bool RangeCounting::remove(const Range & r)
 
 const std::vector<Range> & RangeCounting::ranges(bool splitOncountDiff) const
 {
-  if(recompute_ || recomputeSplit_)
+  if(!recompute_ && !recomputeSplit_)
   {
-    intervals_.clear();
-    int depth = 0;
-    if(limits_.empty())
-    {
-      recompute_ = false;
-      recomputeSplit_ = false;
-      return intervals_;
-    }
-    if(splitOncountDiff)
-    {
-      auto next = [&depth, this](auto & it) {
-        const Limit & v = *it;
-        while(it != limits_.end() && *it == v)
-        {
-          depth -= it->type_;
-          ++it;
-        }
-      };
-      auto s = limits_.cbegin();
-      auto it = s;
-      next(it);
-      while(it != limits_.end())
-      {
-        if(s->i_ != it->i_)
-          intervals_.emplace_back(s->i_, it->i_ - s->i_);
-        else
-          assert((s->type_ == Limit::Lower && it->type_ == Limit::Cut)
-                 || (s->type_ == Limit::Cut && it->type_ == Limit::Upper));
-        s = it;
-        next(it);
-        if(depth == 0 && it != limits_.end())
-        {
-          s = it;
-          next(it);
-        }
-      }
-      recompute_ = true;
-      recomputeSplit_ = false;
-    }
-    else
-    {
-      auto s = limits_.cbegin();
-      for(auto it = limits_.cbegin(); it != limits_.cend(); ++it)
+    return intervals_;
+  }
+
+  intervals_.clear();
+  int depth = 0;
+  if(limits_.empty())
+  {
+    recompute_ = false;
+    recomputeSplit_ = false;
+    return intervals_;
+  }
+  if(splitOncountDiff)
+  {
+    auto next = [&depth, this](auto & it) {
+      const Limit & v = *it;
+      while(it != limits_.end() && *it == v)
       {
         depth -= it->type_;
-        if(depth == 0)
-        {
-          intervals_.emplace_back(s->i_, it->i_ - s->i_);
-          s = std::next(it);
-        }
+        ++it;
       }
-      recompute_ = false;
-      recomputeSplit_ = true;
+    };
+    auto s = limits_.cbegin();
+    auto it = s;
+    next(it);
+    while(it != limits_.end())
+    {
+      if(s->i_ != it->i_)
+        intervals_.emplace_back(s->i_, it->i_ - s->i_);
+      else
+        assert((s->type_ == Limit::Lower && it->type_ == Limit::Cut)
+                || (s->type_ == Limit::Cut && it->type_ == Limit::Upper));
+      s = it;
+      next(it);
+      if(depth == 0 && it != limits_.end())
+      {
+        s = it;
+        next(it);
+      }
     }
+    recompute_ = true;
+    recomputeSplit_ = false;
+  }
+  else
+  {
+    auto s = limits_.cbegin();
+    for(auto it = limits_.cbegin(); it != limits_.cend(); ++it)
+    {
+      depth -= it->type_;
+      if(depth == 0)
+      {
+        intervals_.emplace_back(s->i_, it->i_ - s->i_);
+        s = std::next(it);
+      }
+    }
+    recompute_ = false;
+    recomputeSplit_ = true;
   }
 
   return intervals_;
@@ -171,9 +180,9 @@ void RangeCounting::insert(const Limit & val, It & it, int & depth)
       {
         if(auto n = std::next(it); n != limits_.end() && *n == Limit(val.i_, Limit::Upper))
         {
-          // insertion of (i,+) before (i,|) (i,-): (i,+) is not inserted and (i,-) is removed.
+          // insertion of (i,+) before (i,|) (i,-): (i,+) is not inserted and (i,-) is removed (rule (4))
           if(depth == 0)
-            limits_.erase(it);
+            limits_.erase(it); // enforce rule (6)
           it = limits_.erase(n);
         }
         else
@@ -185,9 +194,9 @@ void RangeCounting::insert(const Limit & val, It & it, int & depth)
       }
       else
       {
-        // insertion of (i,+) before (i,-): (i,-) is changed to (i,|)
+        // insertion of (i,+) before (i,-): (i,-) is changed to (i,|) (rule (3))
         if(depth == 0)
-          it = limits_.erase(it);
+          it = limits_.erase(it); // enforce rule (6)
         else
         {
           it->type_ = Limit::Cut;
@@ -214,10 +223,10 @@ void RangeCounting::insert(const Limit & val, It & it, int & depth)
       {
         if(p != limits_.begin() && *std::prev(p) == Limit(val.i_, Limit::Lower))
         {
-          // insertion of (i,-) after (i,+) (i,|) : (i,-) is not inserted and (i,+) is removed
+          // insertion of (i,-) after (i,+) (i,|) : (i,-) is not inserted and (i,+) is removed (rule (4))
           limits_.erase(std::prev(p));
           if(depth == 0)
-            it = limits_.erase(p);
+            it = limits_.erase(p); // enforce rule (6)
           --depth;
         }
         else
@@ -229,9 +238,9 @@ void RangeCounting::insert(const Limit & val, It & it, int & depth)
       }
       else
       {
-        // insertion of (i,-) after (i,+): (i,+) is changed to (i,|)
+        // insertion of (i,-) after (i,+): (i,+) is changed to (i,|) (rule (3))
         if(depth == 1)
-          limits_.erase(p);
+          limits_.erase(p); // enforce rule (6) (insertion of (i,-) decrease depth so that (i,|) would be at depth=0)
         else
           p->type_ = Limit::Cut;
         --depth;
