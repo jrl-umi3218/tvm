b4ac1572beb34b74918ce3e81564f944b181b500
Adrien Escande <adrien.escande@aist.go.jp>
2022-01-17 17:43:34 +0900

    HierarchicalLeastSquareSolver test with inequalities

--- tests/HierarchicalLeastSquareSolverTest.cpp
+++ tests/HierarchicalLeastSquareSolverTest.cpp
@@ -14,7 +14,7 @@
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
-#include "doctest/doctest.h"
+#include "doctest/EigenDoctest.h"
 
 using namespace tvm;
 using namespace tvm::constraint;
@@ -51,14 +51,18 @@ TEST_CASE("LexLSHierarchicalLeastSquareSolver")
   auto c1c = std::make_shared<BasicLinearConstraint>(A1.row(2), x, b1.tail(1), constraint::Type::EQUAL);
   auto c2 = std::make_shared<BasicLinearConstraint>(A2, x, b2, constraint::Type::EQUAL);
 
+  auto r0 = std::make_shared<SolvingRequirementsWithCallbacks>(PriorityLevel(0));
+  auto r1 = std::make_shared<SolvingRequirementsWithCallbacks>(PriorityLevel(1));
+  auto r2 = std::make_shared<SolvingRequirementsWithCallbacks>(PriorityLevel(2));
+
   LexLSHierarchicalLeastSquareSolver solver(LexLSHLSSolverOptions{});
   VariableVector vars(x);
   solver.startBuild(vars, {3, 3, 3}, {0, 0, 0}, false);
-  solver.addConstraint(0, c0);
-  solver.addConstraint(1, c1a);
-  solver.addConstraint(1, c1b);
-  solver.addConstraint(1, c1c);
-  solver.addConstraint(2, c2);
+  solver.addConstraint(c0, r0);
+  solver.addConstraint(c1a, r1);
+  solver.addConstraint(c1b, r1);
+  solver.addConstraint(c1c, r1);
+  solver.addConstraint(c2, r2);
   solver.finalizeBuild();
 
   solver.solve();
@@ -73,7 +77,7 @@ TEST_CASE("LexLSHierarchicalLeastSquareSolver")
   VectorXd dx2 = pinv(A2 * P1) * (b2 - A2 * (dx0 + dx1));
   VectorXd x0 = dx0 + dx1 + dx2;
 
-  FAST_CHECK_UNARY(x0.isApprox(solver.result(), 1e-10));
+  FAST_CHECK_EQ(solver.result(), Approx(x0).epsilon(1e-10));
 }
 
 TEST_CASE("LexLSHierarchicalLeastSquareSolver min norm")
@@ -91,11 +95,14 @@ TEST_CASE("LexLSHierarchicalLeastSquareSolver min norm")
   auto c0 = std::make_shared<BasicLinearConstraint>(A0, x, b0, constraint::Type::EQUAL);
   auto c1 = std::make_shared<BasicLinearConstraint>(A1, x, b1, constraint::Type::EQUAL);
 
+  auto r0 = std::make_shared<SolvingRequirementsWithCallbacks>(PriorityLevel(0));
+  auto r1 = std::make_shared<SolvingRequirementsWithCallbacks>(PriorityLevel(1));
+
   LexLSHierarchicalLeastSquareSolver solver(LexLSHLSSolverOptions{});
   VariableVector vars(x);
   solver.startBuild(vars, {3, 3, 6}, {0, 0, 0}, false);
-  solver.addConstraint(0, c0);
-  solver.addConstraint(1, c1);
+  solver.addConstraint(c0, r0);
+  solver.addConstraint(c1, r1);
   solver.setMinimumNorm(); // Equivalent to A2 = I and b2 = 0
   solver.finalizeBuild();
 
@@ -111,42 +118,63 @@ TEST_CASE("LexLSHierarchicalLeastSquareSolver min norm")
   VectorXd dx2 = pinv(P1) * (-(dx0 + dx1));
   VectorXd x0 = dx0 + dx1 + dx2;
 
-  FAST_CHECK_UNARY(x0.isApprox(solver.result(), 1e-10));
+  FAST_CHECK_EQ(solver.result(), Approx(x0).epsilon(1e-10));
 }
 
 TEST_CASE("HierarchicalLeastSquares")
 {
-  VariablePtr x = Space(6).createVariable("x");
-  MatrixXd A0(3, 6), A1(3, 6), A2(3, 6);
-  VectorXd b0(3), b1(3), b2(3);
-  // rank(A0)=2
-  A0 = MatrixXd::Random(3, 2) * MatrixXd::Random(2, 6);
-  // rank(A1) = 3 but rank(A1 projected on the nullspace of A0) = 2
-  A1 << MatrixXd::Random(1, 3) * A0, MatrixXd::Random(2, 6);
-  A1 = MatrixXd::Random(3, 3) * A1;
-  A2.setRandom();
-  b0.setRandom();
-  b1.setRandom();
-  b2.setRandom();
+  SUBCASE("Equality only")
+  {
+    VariablePtr x = Space(6).createVariable("x");
+    MatrixXd A0(3, 6), A1(3, 6), A2(3, 6);
+    VectorXd b0(3), b1(3), b2(3);
+    // rank(A0)=2
+    A0 = MatrixXd::Random(3, 2) * MatrixXd::Random(2, 6);
+    // rank(A1) = 3 but rank(A1 projected on the nullspace of A0) = 2
+    A1 << MatrixXd::Random(1, 3) * A0, MatrixXd::Random(2, 6);
+    A1 = MatrixXd::Random(3, 3) * A1;
+    A2.setRandom();
+    b0.setRandom();
+    b1.setRandom();
+    b2.setRandom();
 
-  LinearizedControlProblem pb;
-  pb.add(A0 * x - b0 == 0., {PriorityLevel(0)});
-  pb.add(A1 * x - b1 == 0., {PriorityLevel(1)});
-  pb.add(A2 * x - b2 == 0., {PriorityLevel(2)});
+    LinearizedControlProblem pb;
+    pb.add(A0 * x - b0 == 0., {PriorityLevel(0)});
+    pb.add(A1 * x - b1 == 0., {PriorityLevel(1)});
+    pb.add(A2 * x - b2 == 0., {PriorityLevel(2)});
 
-  scheme::HierarchicalLeastSquares solver(LexLSHLSSolverOptions{});
+    scheme::HierarchicalLeastSquares solver(LexLSHLSSolverOptions{});
 
-  solver.solve(pb);
+    solver.solve(pb);
 
-  // Compute the solution by hand
-  MatrixXd P0 = MatrixXd::Identity(6, 6) - pinv(A0) * A0;
-  MatrixXd A01(6, 6);
-  A01 << A0, A1;
-  MatrixXd P1 = MatrixXd::Identity(6, 6) - pinv(A01) * A01;
-  VectorXd dx0 = pinv(A0) * b0;
-  VectorXd dx1 = pinv(A1 * P0) * (b1 - A1 * dx0);
-  VectorXd dx2 = pinv(A2 * P1) * (b2 - A2 * (dx0 + dx1));
-  VectorXd x0 = dx0 + dx1 + dx2;
+    // Compute the solution by hand
+    MatrixXd P0 = MatrixXd::Identity(6, 6) - pinv(A0) * A0;
+    MatrixXd A01(6, 6);
+    A01 << A0, A1;
+    MatrixXd P1 = MatrixXd::Identity(6, 6) - pinv(A01) * A01;
+    VectorXd dx0 = pinv(A0) * b0;
+    VectorXd dx1 = pinv(A1 * P0) * (b1 - A1 * dx0);
+    VectorXd dx2 = pinv(A2 * P1) * (b2 - A2 * (dx0 + dx1));
+    VectorXd x0 = dx0 + dx1 + dx2;
 
-  FAST_CHECK_UNARY(x0.isApprox(x->value(), 1e-10));
+    FAST_CHECK_EQ(x->value(), Approx(x0).epsilon(1e-10));
+  }
+
+  SUBCASE("Inequality only")
+  {
+    VariablePtr x = Space(1).createVariable("x");
+    VariablePtr y = Space(1).createVariable("y");
+
+    LinearizedControlProblem pb;
+    pb.add(-1. <= x <= 1., {PriorityLevel(0)});
+    pb.add( 2. <= x <= 3., {PriorityLevel(1)});
+    pb.add(-1. <= y <= 1., {PriorityLevel(2)});
+    pb.add(x + y <= 0., {PriorityLevel(4)});
+
+    scheme::HierarchicalLeastSquares solver(LexLSHLSSolverOptions().feasibleFirstLevel(true));
+
+    solver.solve(pb);
+    FAST_CHECK_EQ(x->value()[0], doctest::Approx(1).epsilon(1e-10));
+    FAST_CHECK_EQ(y->value()[0], doctest::Approx(-1).epsilon(1e-10));
+  }
 }
